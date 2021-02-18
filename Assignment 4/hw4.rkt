cd58b0eca6fab17ff9750e1b0ff92fff6ec8097e
GrayGorilla <nbren004@ucr.edu>
2021-02-17 19:24:41 -0800

    completed 4.2 - string-append-map

--- Assignment 4/hw4.rkt
+++ Assignment 4/hw4.rkt
@@ -1,12 +1,16 @@
-
 #lang racket
 
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
 ;; put your code below
 
+
 ;; 1
 (define (sequence spacing low high)
   (cond [(= low high) (list low)]
         [(> low high) (list)]  ; empty list
         [#t (cons low (sequence spacing (+ low spacing) high))]))
+
+;; 2
+(define (string-append-map xs suffix)
+  (map (lambda (x) (string-append x suffix)) xs))
