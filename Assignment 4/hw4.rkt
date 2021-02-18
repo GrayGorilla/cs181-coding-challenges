e422c6de8f325d83c653dfc21278d458bdb51b4e
GrayGorilla <nbren004@ucr.edu>
2021-02-17 16:43:50 -0800

    completed 4.1 - sequence

--- Assignment 4/hw4.rkt
+++ Assignment 4/hw4.rkt
@@ -4,3 +4,11 @@
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
 ;; put your code below
+
+;; 1
+(define (sequence spacing low high)
+  (if (= low high)
+    (list low)
+    (if (> low high)
+        (list)  ;; empty list
+        (cons low (sequence spacing (+ low spacing) high)))))
