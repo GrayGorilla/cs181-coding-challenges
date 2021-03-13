ac60f7d5888d2d4fefceb745bb3873e8d4453479
GrayGorilla <nbren004@ucr.edu>
2021-03-12 22:00:19 -0800

    added 3 new Tetris pieces; modified code to remain stable with new piece sizes

--- Assignment 6/hw6assignment.rb
+++ Assignment 6/hw6assignment.rb
@@ -5,18 +5,73 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
+  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],                  # square (only needs one)
+               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]),           # T
+               [[[0, 0], [-1, 0], [1, 0], [2, 0]],                      # long (only needs two)
+               [[0, 0], [0, -1], [0, 1], [0, 2]]],
+               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]),            # L
+               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]),           # inverted L
+               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]),          # S
+               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]),          # Z
+               rotations([[0, 0], [-1, 0], [1, 0], [0, -1], [-1, -1]]), # Big new piece
+               [[[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],             # Long new piece (only needs two)
+               [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]],
+               rotations([[0, 0], [1, 0], [0, -1]])]                    # Short L new piece
 
   # your enhancements here
 
+  # class method to choose the next piece
+  def self.next_piece (board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
 end
 
 class MyBoard < Board
   # your enhancements here
+  def initialize (game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
+
+  # gets the next piece
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
+  end
+
+  # gets the information from the current piece about where it is and uses this
+  # to store the piece on the board itself.  Then calls remove_filled.
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    sizeEnd = locations.length - 1
+
+    (0..sizeEnd).each{|index|       # change loop # so it can vary depending on piece size
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
 
 end
 
 class MyTetris < Tetris
   # your enhancements here
+
+  # creates a canvas and the board that interacts with it
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
   def key_bindings  
     @root.bind('n', proc {self.new_game}) 
 
